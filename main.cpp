70047167229f2dd02cc03595c6d33db9d80280fc
Abe <abenanduri1@gmail.com>
2024-11-06 18:22:31 -0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// include all the headers to be used, map, list, array, iostream, fstream 
+
+// function declration to be called when handling time based events of what is being imported, 
+// exported and what is being produced and sold domestically
+
+//main function begins here
+    // declare a map with a country as the key and the values are an array of 3 lists, imports, exports and 
+    // domestically sold products
+
+    //open the file and include an error message if it fails to open
+
+    //read country names and some list of items that are traded into the map, and the items into the lists
+
+    
