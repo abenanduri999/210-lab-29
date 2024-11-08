741d242cdee47a3362a2d8131a88ceedc3cf963d
Abe <abenanduri1@gmail.com>
2024-11-08 12:41:07 -0800

    update 17

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include<array>
 #include<string> 
 #include<fstream>
+#include<vector> 
 
 
 using namespace std;
@@ -24,36 +25,10 @@ void trade(map<string, list<string>[T_COUNT]>&);
     display(countryData); 
     cout<<endl; 
     trade(countryData); 
-    //display(countryData); 
-       
-
-
-    //open the file and include an error message if it fails to open
-
-
-    //run a simulation 25 times and go through every country 
-        // call the function we declared earlier
-       
-        //test loop to iterate thorough the small map we have and call the function for every value in the 
-        // map 
-        //in the fucntion we run the probability of what new item they either import, exported, or sold 
-        // domestically
-        // the item is chosen at random and addded to the appropriate list 
-
-    // return 0 to end the main function
+        
     return 0; 
     }
 
-// the function is written out here
-
-    // use a for loop to run through the suimulation 25 times
-        //use srand time to keep getting a new int 
-        //set up probability of an event happening, based on that determine whether a new import 
-        // export or domestic product has been added
-        // set up a rare case where all exports cease for a certain country due to 
-        //shortages and the list of exports get 
-        // added to the domestic products sold only 
-    // dummy parameters go into teh function to make sure the functin call works
 void populate(map<string, list<string>[T_COUNT]>& countdt)
 {
      ifstream input1("countries.txt"); 
@@ -120,6 +95,15 @@ void display(map<string, list<string>[T_COUNT]> cdmap)
 
 void trade(map<string, list<string>[T_COUNT]>& counttrad)
 {
+    vector<string>newprod; 
+
+    fstream input3("products3.txt");
+    string newp; 
+    while(input3>>newp)
+    {
+        newprod.push_back(newp); 
+    }
+
     srand(time(0)); 
 
     for (auto e : counttrad) 
@@ -127,21 +111,26 @@ void trade(map<string, list<string>[T_COUNT]>& counttrad)
        int prob = rand() % 100 + 1;
         if (prob <= 20)
         {
-            counttrad[0][0].pop_back(); 
+            e.second[0].pop_back(); 
 
         } 
         prob = rand() % 100 + 1;
         if (prob <= 30)
         {
-            counttrad[0][1].pop_back(); 
+            e.second[1].pop_back(); 
 
         } 
         prob = rand() % 100 + 1;
         if (prob <= 40)
         {
-            counttrad[0][2].pop_back(); 
-
+            e.second[2].pop_back();
         } 
+        prob = rand() % 100 + 1;
+        if(prob <= 50)
+        {
+            int newp = rand() % newprod.size() + 1; 
+            e.second[0].push_back(newprod[newp]); 
+        }
         
   
         display(counttrad); 
